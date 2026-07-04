156bfee1f54abb861b321d49f28da5716acf0c64
arunak160897 <arunak160897@gmail.com>
2026-07-04 20:36:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function hello(){
-alert("Thank you for visiting Arun Kumar's website!");
+function hello() {
+    alert("Thank you for visiting my website!");
 }
+
+window.onload = function () {
+    console.log("Website Loaded Successfully");
+}
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+    card.addEventListener("mouseover", function () {
+        card.style.transform = "translateY(-10px) scale(1.05)";
+    });
+
+    card.addEventListener("mouseout", function () {
+        card.style.transform = "translateY(0) scale(1)";
+    });
+});
